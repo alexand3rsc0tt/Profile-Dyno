4d866342021f686987d615aac239c22780fb0b01
alexand3rsc0tt <alex.burton1993@gmail.com>
2014-11-17 11:03:54 -0700

    removed what I just did

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,3 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-config.serve_static_assets = true
-config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-config.assets.compile = true
